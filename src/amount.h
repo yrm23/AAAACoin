02fbe2afb2a5f621ac286df4c317f28ed6bbdf6b
yrm23 <34798104+yrm23@users.noreply.github.com>
2018-05-17 11:17:07 -0400

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -14,6 +14,9 @@ typedef int64_t CAmount;
 static const CAmount COIN = 100000000;
 static const CAmount CENT = 1000000;
 
+// COIN = SHELL; SAND = 1;
+
+
 /** No amount larger than this (in satoshi) is valid.
  *
  * Note that this constant is *not* the total money supply, which in Bitcoin
