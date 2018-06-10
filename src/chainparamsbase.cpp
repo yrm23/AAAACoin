2ec8967e309a70695bef017458b7854e47e0fb68
yrm23 <34798104+yrm23@users.noreply.github.com>
2018-06-09 20:06:25 -0400

    Update chainparamsbase.cpp

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 9567;                                     ////////////////
+        nRPCPort = 9566;                                     ////////////////
     }
 };
 
@@ -44,7 +44,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19567;                                   //////////////
+        nRPCPort = 19566;                                   //////////////
         strDataDir = "testnet3";
     }
 };
@@ -57,7 +57,7 @@ class CBaseRegTestParams : public CBaseChainParams
 public:
     CBaseRegTestParams()
     {
-        nRPCPort = 19678;                                   /////////////
+        nRPCPort = 19677;                                   /////////////
         strDataDir = "regtest";
     }
 };
