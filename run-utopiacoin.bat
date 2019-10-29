0f0f6c9a87d4b26844d4a02d8263886c72736ca8
yrm23 <34798104+yrm23@users.noreply.github.com>
2019-10-29 08:22:31 -0400

    Update run-utopiacoin.bat

--- run-utopiacoin.bat
+++ run-utopiacoin.bat
@@ -1,7 +1,8 @@
 
-cd C:.\Desktop\UtopiaChain\bin
+cd C:\Users\yongr_g57dnkt\Desktop\UtopiaChain\bin
 
-utopiacoin-qt.exe -conf="C:.\Desktop\UtopiaChain\utopiacoin.txt"
+utopiacoin-qt.exe -conf="C:\Users\yongr_g57dnkt\Desktop\UtopiaChain\utopiacoin.txt"
  
  
- // put both of above at same directory folder like .../Desktop or .../Documents. 
+ // 把下载并解压缩的UtopiaChain\bin和utopiacoin.txt放置同一个文件夹内如Desktop或Documents。然后用该文件夹的完整地址置换上述两个指令的
+ //                              C:\Users\yongr_g57dnkt\Desktop部分。保存即可。
