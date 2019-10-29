3922a1a84829a3b67a7a1f07b5556bd8095cdf51
yrm23 <34798104+yrm23@users.noreply.github.com>
2019-10-29 08:12:37 -0400

    Update run-utopiacoin.bat

--- run-utopiacoin.bat
+++ run-utopiacoin.bat
@@ -2,3 +2,6 @@
 cd C:.\Desktop\UtopiaChain\bin
 
 utopiacoin-qt.exe -conf="C:.\Desktop\UtopiaChain\utopiacoin.txt"
+ 
+ 
+ // put both of above at same directory folder like .../Desktop or .../Documents. 
