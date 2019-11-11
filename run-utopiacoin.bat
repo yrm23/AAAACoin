d503b602b93dbf9cb782280fa1f8f3fb8233330b
yrm23 <34798104+yrm23@users.noreply.github.com>
2019-11-10 21:53:25 -0500

    Delete run-utopiacoin.bat

--- run-utopiacoin.bat
+++
@@ -1,9 +0,0 @@
-
-cd C:\Users\yongr_g57dnkt\Desktop\UtopiaChain\bin
-
-utopiacoin-qt.exe -conf="C:\Users\yongr_g57dnkt\Desktop\UtopiaChain\utopiacoin.txt"
- 
- REM                       本文件的打开方式是：点右键 -> 编辑。
- REM                       把下载并解压缩的UtopiaChain\bin和utopiacoin.txt放置同一个文件夹内如Desktop或Documents。
- REM                       然后用该文件夹的完整地址置换上述两个指令的C:\Users\yongr_g57dnkt\Desktop部分。保存即可。
- REM                       这里的REM是一个符号，用来标明注释行的符号，没有其他意义。
